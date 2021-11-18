c96ed52d25dab32745ac56d381402962c05bd782
Amil Rustamov <amil93.rustamov@gmail.com>
2021-11-18 23:55:55 +0400

    unusuable spaces is deleted

--- src/App.tsx
+++ src/App.tsx
@@ -12,20 +12,6 @@ function App() {
   const { isStart ,selectValue} = useGlobalData();
   const questions= useRefactorAnswers(selectValue.category,selectValue.level);
   console.log('app rendered')
-//   const {data}=useFetchQuiz(selectValue.category,selectValue.level);
-//   const question =data?.data.results;
-//   const [questions,setQuestions]=useState<NewData[]>(question)
-//   useEffect(()=>{
-//     if(data){
-//              question.map((question:NewData)=>(
-//            {...question,answer:[...question.incorrect_answers,question.correct_answer].sort(() => Math.random() - 0.5)}
-//        ));
-//        console.log('question',question)
-//    }
-//    setQuestions(question);
-//    },[selectValue])
-// console.log(questions);
-
   return (
    
       <div className={classes.app}>
